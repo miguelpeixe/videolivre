d0cef8f2682536f62f95522d2fdbdee335007955
Miguel Peixe <miguel@cardume.art.br>
2013-06-28 00:58:12 -0300

    fix template structure

--- videolivre-channel/single.php
+++ videolivre-channel/single.php
@@ -3,7 +3,7 @@
 <div id="primary" class="site-content">
 	<div id="content" role="main">
 		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-			<?php get_template_part(get_post_type(), 'content'); ?>
+			<?php get_template_part(get_post_type(), 'full'); ?>
 		<?php endwhile; endif; ?>
 	</div>
 </div>
